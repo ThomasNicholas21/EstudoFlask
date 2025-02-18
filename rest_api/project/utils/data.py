68d008949595feb75ea6ebdd1f7bf22c86ee8b86
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-18 18:21:38 -0300

    feat: implementando método de busca para dicionário

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -2,14 +2,17 @@
 
 def get_developer(data_developer, list_developer):
     for devs in list_developer:
-        if data_developer == devs:
-            return devs
-    
-    return 'Developer Not Found'
+        for value in devs.values():
+            if value == data_developer:
+                return value
+
+            return 'Developer not Found'
+
+
 
 def post_developer_name(data_developer, list_developer):
     default_dict = {}
     default_dict.setdefault('name', data_developer)
     list_developer.append(default_dict)
-
+    
     return list_developer
