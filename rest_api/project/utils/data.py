e5127b860cf0df8d903eb759489d46e744f496df
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-17 22:55:19 -0300

    feat: implementando função para post de developer name

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -6,3 +6,10 @@ def get_developer(data_developer, list_developer):
             return devs
     
     return 'Developer Not Found'
+
+def post_developer_name(data_developer, list_developer):
+    default_dict = {}
+    default_dict.setdefault('name', data_developer)
+    list_developer.append(default_dict)
+
+    return list_developer
