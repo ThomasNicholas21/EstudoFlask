55651525a33b76c1298c538099fa44826d7d8060
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-17 22:47:32 -0300

    feat: implementando get_developer para buscar um desenvolvedor dentro de uma lista

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -1,1 +1,8 @@
 # aqui icorrerá processamento de dados vindo da request do tipo "POST"
+
+def get_developer(data_developer, list_developer):
+    for devs in list_developer:
+        if data_developer == devs:
+            return devs
+    
+    return 'Developer Not Found'
