2af3156ae1d9f90f25ddff844c5eccce252e153e
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-17 22:02:21 -0300

    feat: criando endpoint para dev  e implementando lista de armazenamento

--- rest_api/project/API/dev_view.py
+++ rest_api/project/API/dev_view.py
@@ -1,7 +1,12 @@
 from flask import Blueprint
 
 devs = Blueprint('home', __name__)
+LISTA_DEV = []
 
-@devs.route('/')
+@devs.route('/', methods=['GET'])
 def home():
-    return 'Testando'
+    return LISTA_DEV
+
+@devs.route('/<string:developer>')
+def dev_name(developer):
+    return 'developer'
