e6cb03545b6e2c95f749227a77284e167ab8df25
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-18 22:49:59 -0300

    feat: implementado requeste.data para receber varias tecnologias

--- rest_api/project/API/dev_view.py
+++ rest_api/project/API/dev_view.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 from project.utils.data import get_developer, post_developer_name
+from project.utils.data import post_developer_tecnologie
+import json
 
 devs = Blueprint('home', __name__)
 DEV_LIST = []
@@ -24,13 +26,18 @@ def dev_name(developer):
     
     return 'Invalid Method'
 
-@devs.route('/<string:developer>/<string:tecnologies>', methods=['GET', 'POST'])
-def dev_tecnologies(developer, tecnologies):
+@devs.route('/<string:developer>/tecnologies', methods=['GET', 'POST'])
+def dev_tecnologies(developer):
+    tecnologies = json.loads(request.data)
+
     if request.method == 'GET':
         return 'GET'
     
     elif request.method == 'POST':
-        return 'POST'
+
+        post_developer_tecnologie(developer, tecnologies, DEV_LIST)
+
+        return 'processamento'
     
     return 'Invalid Method'
 
