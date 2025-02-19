7a671c0ebdc92cb92dff7d31e507884e06147cdd
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-19 20:58:02 -0300

    feat: criando método put

--- rest_api/project/API/dev_view.py
+++ rest_api/project/API/dev_view.py
@@ -34,3 +34,14 @@ def get_dev_name(id):
 
     return 'Invalid Method'
 
+
+@devs.route('/dev/<int:id>', methods=['PUT'])
+def get_dev_name(id):
+    if request.method == 'GET':
+        
+        get_dev_name = get_developer(id, DEV_LIST)
+
+        return jsonify(get_dev_name)
+    
+
+    return 'Invalid Method'
