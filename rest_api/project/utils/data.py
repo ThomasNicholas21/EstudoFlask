28e96eecc2219ff020091d51d647d8e45b791926
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-18 22:05:39 -0300

    refactor: refatorando modo de armazenamento

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -11,14 +11,26 @@ def get_developer(data_developer, list_developer):
 
 
 def post_developer_name(data_developer, list_developer):
-    default_dict = {}
-    default_dict.setdefault('name', data_developer)
+    default_dict = {'name': data_developer}
     list_developer.append(default_dict)
     
-    return list_developer
 
 def get_devoloper_tecnologie():
     ...
 
 def post_developer_tecnologie():
     ...
+
+LIST_DEV = []
+valor1 = 'thomas'
+valor2 = 'asdsad'
+valor3 = 'ddddd'
+
+valor_t = 'tecnologia'
+
+post_developer_name(valor1, LIST_DEV)
+post_developer_name(valor2, LIST_DEV)
+post_developer_name(valor3, LIST_DEV)
+
+print(LIST_DEV)
+# info_tec = post_developer_tecnologie(valor3, valor_t, LIST_DEV)
