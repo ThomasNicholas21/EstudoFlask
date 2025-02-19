07e93c06bbf196fe2ec8ac6df4c2f7a589812cd0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-18 23:18:56 -0300

    feat: tipando post_developer_tecnologie

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -13,7 +13,7 @@ def post_developer_name(data_developer: dict, list_developer: list[dict]):
     list_developer.append(default_dict)
 
 
-def post_developer_tecnologie(data_developer, data_tecnologie, list_developer):
+def post_developer_tecnologie(data_developer: dict, data_tecnologie: dict, list_developer: list[dict]):
     get_developer_info = get_developer(data_developer, list_developer)
     if get_developer_info:
         get_developer_info.update(data_tecnologie)
