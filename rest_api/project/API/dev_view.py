e99e4a83be82e1a18af9fac293020acf58691842
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-19 20:57:13 -0300

    feat: criando método post

--- rest_api/project/API/dev_view.py
+++ rest_api/project/API/dev_view.py
@@ -22,3 +22,15 @@ def get_dev_name(id):
 
     return 'Invalid Method'
 
+
+@devs.route('/dev/post', methods=['POST'])
+def get_dev_name(id):
+    if request.method == 'GET':
+        
+        get_dev_name = get_developer(id, DEV_LIST)
+
+        return jsonify(get_dev_name)
+    
+
+    return 'Invalid Method'
+
