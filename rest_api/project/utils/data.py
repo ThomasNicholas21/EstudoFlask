3bfd045b4ad110f4a11fb87e7aae790fedfb65bf
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-19 21:27:38 -0300

    refactor: retirando função tecnologie

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -15,9 +15,3 @@ def post_developer_name(data_developer: dict, list_developer: list[dict]):
 def update_developer_name(data_developer: dict, developer_id:int, list_developer: list[dict]):
     list_developer[developer_id] = data_developer
 
-
-def post_developer_tecnologie(data_developer: dict, data_tecnologie: dict, list_developer: list[dict]):
-    get_developer_info = get_developer(data_developer, list_developer)
-    if get_developer_info:
-        get_developer_info.update(data_tecnologie)
-        
