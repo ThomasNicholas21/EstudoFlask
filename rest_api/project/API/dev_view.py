8c59ffdd4d8f7eca1f762b8490130db3b06302bc
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-19 21:58:12 -0300

    chore: melhorando identação

--- rest_api/project/API/dev_view.py
+++ rest_api/project/API/dev_view.py
@@ -3,8 +3,10 @@ from project.utils.data import get_developer, post_developer_name
 from project.utils.data import update_developer_name, delete_developer
 import json
 
+
 devs: Blueprint = Blueprint('home', __name__)
 DEV_LIST = []
+
 
 @devs.route('/', methods=['GET'])
 def home():
@@ -14,11 +16,9 @@ def home():
 @devs.route('/dev/<int:id>', methods=['GET'])
 def get_dev_name(id):
     if request.method == 'GET':
-        
         get_dev_name = get_developer(id, DEV_LIST)
 
         return jsonify(get_dev_name)
-    
 
     return 'Invalid Method'
 
@@ -28,12 +28,10 @@ def post_dev_name():
     developer = json.loads(request.data)
 
     if request.method == 'POST':
-        
         post_developer_name(developer, DEV_LIST)
 
         return 'Developer Created'
     
-
     return 'Invalid Method'
 
 
@@ -42,22 +40,18 @@ def update_dev_name(id):
     developer = json.loads(request.data)
 
     if request.method == 'PUT':
-        
         update_developer_name(developer, id, DEV_LIST)
 
         return 'User updated'
     
-
     return 'Invalid Method'
 
 
 @devs.route('/dev/<int:id>/delete', methods=['DELETE'])
 def delete_dev_name(id):
     if request.method == 'DELETE':
-        
         delete_developer(id, DEV_LIST)
 
         return 'Developer Deleted'
     
-
     return 'Invalid Method'
