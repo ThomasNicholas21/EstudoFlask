6c98a94b226bd08675f7fa9fd1ec611df094065f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-19 21:12:56 -0300

    feat: implementando get developer e indexerror

--- rest_api/project/utils/data.py
+++ rest_api/project/utils/data.py
@@ -1,16 +1,17 @@
 # aqui icorrerá processamento de dados vindo da request do tipo "POST"
 
 def get_developer(developer_id: int, list_developer: list[dict]):
-    for value in list_developer:
-        if value == developer_id:
-            return value
-    
-    return None
+    try:
+        developer = list_developer[developer_id]
+        return developer
+    except IndexError:
+        raise IndexError(f'Error: list has none value: {developer} ')
 
 
 def post_developer_name(data_developer: dict, list_developer: list[dict]):
     default_dict = {'name': data_developer}
     list_developer.append(default_dict)
+
 
 
 def post_developer_tecnologie(data_developer: dict, data_tecnologie: dict, list_developer: list[dict]):
